2c460bca18b4788b7650a7b6c12c8feb10133fb1
Amr moussa <amrmoussa2016@icloud.com>
2020-03-30 01:17:07 +0200

    Initial Commit

---
+++ Roll_dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roll_dice
+//
+//  Created by Amr Moussa on 3/30/20.
+//  Copyright © 2020 Amr Moussa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
