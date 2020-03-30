381e9be09fd637b24982af83dbc30591b8d963d5
Amr moussa <amrmoussa2016@icloud.com>
2020-03-30 04:52:39 +0200

    roll a dice

---
+++ Roll_dice/README.md
@@ -0,0 +1,10 @@
+/* 
+  README.md
+  Roll_dice
+
+  Created by Amr Moussa on 3/30/20.
+  Copyright © 2020 Amr Moussa. All rights reserved.
+*/
+A player  rolls a dice and  the score increases when equal dices numbers
+and here is some shots
+
--- Roll_dice/ViewController.swift
+++ Roll_dice/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    let images = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    var score = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let leftNumber=Int.random(in: 0...5)
+        let rightNumber=Int.random(in: 0...5)
+        leftDiceImageView.image = images[leftNumber]
+        rightDiceImageView.image = images[rightNumber]
+        if leftNumber == rightNumber || (leftNumber == 5 && rightNumber == 5) {
+            score += 1
+        }
+        scoreLabel.text = String(score)
+        
+    }
+    
 }
 
